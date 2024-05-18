04142e9152349e2f71bb5c4cc2f3a63b409782c9
ZhiweiShan <74201033+ZhiweiShan@users.noreply.github.com>
2024-05-19 03:02:59 +0800

    Add files via upload

---
+++ Journal_Compaire.py
@@ -0,0 +1,32 @@
+import subprocess
+import concurrent.futures
+import os
+MAIS = 'Journal/MAIS.py'
+OTHGN = 'Journal/Journal_Run_exact.py'
+OSIA = "Journal/Journal_Run_OSIA.py"
+OVIA = 'Journal/Journal_Run_OVIA.py'
+SSIA = 'Journal/Journal_Run_SSIA.py'
+SVIA = 'Journal/Journal_Run_SVIA.py'
+current_path = os.getcwd()
+
+# 打印当前工作目录
+print("当前工作目录是:", current_path)
+# num_nodes
+args = ['data/new_data_di_ER_6_0.4_1000_test_chromatic','6']
+
+
+commands = [MAIS, OTHGN, OSIA, OVIA, SSIA, SVIA]
+#commands = [OVIA]
+# for command in commands:
+#     print("Running:", command)
+#     subprocess.run(["python", command] + args)
+
+# Parallel running
+def run_command(command):
+    print("Running:", command)
+    subprocess.run(["python", command] + args)
+
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    executor.map(run_command, commands)
+
+
